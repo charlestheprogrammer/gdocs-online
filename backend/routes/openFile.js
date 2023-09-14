db40fba12e2075b64529a0040e9e6b30b8cc622a
charles.simon-meunier <charles.simon-meunier@epita.fr>
2023-09-14 16:46:11 +0200

    Import -> require

--- backend/routes/openFile.js
+++ backend/routes/openFile.js
@@ -1,11 +1,11 @@
-const DocumentSchema = require('./schemas/document');
-const SaveSchema = require('./schemas/save');
+const DocumentSchema = require('../schemas/document');
+const SaveSchema = require('../schemas/save');
 
 var express = require('express');
 var router = express.Router();
 
 /* POST Open a file based on his name. */
-router.post('/', async function(req, res) {
+router.get('/', async function(req, res) {
   var documentFound = await DocumentSchema.findById(req.body.title).exec();
   var saveFound = await SaveSchema.findOne({document: documentFound}).sort({date: -1}).exec();
   res.send({message: "Récupère ton contenu chacal", content: saveFound.content});
