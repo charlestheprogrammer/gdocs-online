48227f6c78dbc9baa15ea14fff3dc7800aef2991
Johnny Chen <johnny.chen@epita.fr>
2023-09-23 21:09:44 +0200

    Homepage front & document access / creation

--- frontend/scripts/toolbar.js
+++ frontend/scripts/toolbar.js
@@ -31,6 +31,8 @@ const imageInput = document.getElementById("import_image");
 
 let saveButton = document.getElementById("save");
 let openButton = document.getElementById("open");
+let showButton = document.getElementById("show");
+let newButton = document.getElementById("new");
 
 const modifyText = (command, defaultUi, value) => {
   document.execCommand(command, defaultUi, value);
@@ -131,6 +133,7 @@ openButton.addEventListener("click", () => {
       res.json().then((body) => {
         let openModal = document.querySelector(".openModal");
         openModal.style.display = "flex";
+        console.log("test")
         let availableFiles = document.getElementById("availableFiles");
         availableFiles.innerHTML = "";
         body.forEach((doc) => {
@@ -148,6 +151,61 @@ openButton.addEventListener("click", () => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch("http://localhost:3000/api/documents")
+  .then((res) => {
+    res.json().then((body) => {
+      let filesModal = document.querySelector(".filesModal");
+      filesModal.style.display = "flex";
+      let homePageFiles = document.getElementById("homePageFiles");
+      homePageFiles.innerHTML = "";
+      let documentList = document.createElement("ul");
+      documentList.classList.add("document-list");
+      body.forEach((doc) => {
+        let documentBlock = document.createElement("div");
+        documentBlock.classList.add("document-block");
+        let documentInfo = document.createElement("div");
+        documentInfo.classList.add("document-info");
+        let title = document.createElement("h2");
+        title.textContent = doc.title;
+        let lastModified = document.createElement("p");
+        lastModified.textContent = `Dernière modification : 21 septembre 2023`;
+        let image = document.createElement("img");
+        image.src = "./assets/gdocs.png";
+        image.classList.add("image-style");
+
+        documentInfo.appendChild(image);
+        documentInfo.appendChild(title);
+        documentInfo.appendChild(lastModified);
+        documentBlock.appendChild(documentInfo);
+        documentList.appendChild(documentBlock);
+        documentBlock.addEventListener("click", () => {
+          openFile(doc._id);
+          let filesModal = document.querySelector(".filesModal");
+          filesModal.style.display = "none";
+        });
+        homePageFiles.appendChild(documentList);
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+newButton.addEventListener("click", () => {
+  document_title = "Nouveau document";
+  document_content_element.innerHTML = "";
+  localStorage.removeItem("idDocument");
+  let filesModal = document.querySelector(".filesModal");
+  filesModal.style.display = "none";
+  updateTitle();
+  document.querySelector(".users_cursors").innerHTML = "";
+  document.querySelectorAll(".connected_user").forEach((user) => {
+    user.remove();
+  });
 });
 
 function openFile(document_id) {
