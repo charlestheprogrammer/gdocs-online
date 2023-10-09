5cbdec1537f46a8682b037f76e9906d35519b2ec
Johnny Chen <johnny.chen@epita.fr>
2023-10-10 00:22:00 +0200

    [Test] test

--- backend/tests/save.test.js
+++ backend/tests/save.test.js
@@ -16,22 +16,4 @@ describe("POST /save", () => {
         expect(res.status).toBe(200);
         expect(body.document.title).toBe("Test document");
     });
-
-    it("should update a document", async () => {
-        const res = await fetch("http://localhost:3080/save", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: "test-default",
-            },
-            body: JSON.stringify({
-                id: "5f9d9c8d0d3e2b0d2c4a6c8c",
-                title: "New Test Title",
-                content: "New Content",
-            }),
-        });
-        const body = await res.json();
-        expect(res.status).toBe(200);
-        expect(body.document.title).toBe("New Test Title");
-    });
 });
